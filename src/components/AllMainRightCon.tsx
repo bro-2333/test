404b91bf1be0ce27011596508ac52ad3403c65e0
shangjie <13213541+shangjiejie@user.noreply.gitee.com>
2023-11-17 11:54:55 +0800

    new allright

--- src/components/AllMainRightCon.tsx
+++ src/components/AllMainRightCon.tsx
@@ -5,8 +5,8 @@ import lunarFestivals from '../data/data'
 import Calendar from './Calendar';
 import Footer from './Footer';
 import Master from './Master';
- import Lbtt from './Lbt'//轮播图
- import Scroll from './Scroll';//滚动
+import Lbtt from './Lbt'//轮播图
+import Scroll from './Scroll';//滚动
 function AllMainRightCon() {
   const { Search } = Input;
   const options = [
@@ -18,7 +18,7 @@ function AllMainRightCon() {
       value: 'H5',
       label: 'H5',
     },
-      {
+    {
       value: '表单',
       label: '表单',
     },
@@ -26,7 +26,7 @@ function AllMainRightCon() {
       value: '长图',
       label: '长图',
     },
-      {
+    {
       value: '互动',
       label: '互动',
     },
@@ -46,20 +46,20 @@ function AllMainRightCon() {
   let str = ['邀请函', '互动游戏', 'H5', '婚礼邀请函']
   let [count, setcount]: any = useState(0);
   const [selectedDate, setSelectedDate] = useState(new Date());
-  const [flag,setflag] = useState(false)
+  const [flag, setflag] = useState(false)
   const onSearch: any = (value: any) => console.log(value);
   const lunarDate = LunarCalendar.solarToLunar(new Date().getFullYear(), new Date().getMonth() + 1, new Date().getDate());
   const lunarMomthTime = lunarDate.lunarMonthName
   const lunarDateTime = lunarDate.lunarDayName
   const key = `${new Date().getMonth() + 1}-${new Date().getDate()}`;
-  const fistival =lunarFestivals[key]?lunarFestivals[key].fast[0]:'每天好心情'
-  let setTimeMouse:any
-  
-  let [defaultValue, setdefaultValue] = useState('邀请函')
-  
+  const fistival = lunarFestivals[key] ? lunarFestivals[key].fast[0] : '每天好心情'
+  let setTimeMouse: any
 
-  const calenDay = useCallback((day:number) => {
-    switch(day) {
+  let [defaultValue, setdefaultValue] = useState('邀请函')
+
+
+  const calenDay = useCallback((day: number) => {
+    switch (day) {
       case 0: return '日';
       case 1: return '一';
       case 2: return '二';
@@ -68,7 +68,7 @@ function AllMainRightCon() {
       case 5: return '五';
       case 6: return '六';
     }
-  },[selectedDate.getDay()])
+  }, [selectedDate.getDay()])
   // 日历
   const handleMonthChange = (change: any) => {
     const newDate = new Date(selectedDate);
@@ -83,7 +83,7 @@ function AllMainRightCon() {
     setflag(true);
   }
   const mouseLeave = () => {
-    setTimeMouse=setTimeout(() => {
+    setTimeMouse = setTimeout(() => {
       setflag(false);
     }, 400);
   }
@@ -132,26 +132,51 @@ function AllMainRightCon() {
             </div>
           </div>
         </div>
-       
-        <div className={flag?'Calendar_box':'Calendar_box_hidden'} onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}>
+
+        <div className={flag ? 'Calendar_box' : 'Calendar_box_hidden'} onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}>
           <div>
-            <button style={{border:'none',margin:'5px'}} onClick={() => handleMonthChange(-1)}>&lt;</button>
+            <button style={{ border: 'none', margin: '5px' }} onClick={() => handleMonthChange(-1)}>&lt;</button>
             <span style={{ fontSize: '20px' }}>{`${selectedDate.getFullYear()}年${selectedDate.getMonth() + 1}月`}</span>
-            <button style={{border:'none',margin:'5px'}} onClick={() => handleMonthChange(1)}>&gt;</button>
+            <button style={{ border: 'none', margin: '5px' }} onClick={() => handleMonthChange(1)}>&gt;</button>
           </div>
           <Calendar year={selectedDate.getFullYear()} month={selectedDate.getMonth() + 1} />
         </div>
       </div>
-        <div className="all_main_swiper">
-       <Lbtt></Lbtt>
-        <Scroll></Scroll>
+
+      {/* 轮播图 */}
+      <div className="all_main_swiper">
+        <Lbtt></Lbtt>
+        <div className="swiper">
+          <Scroll></Scroll>
+        </div>
       </div>
+
+      {/* 广告盒 */}
+      <div className="all_main_ads_box">
+        <img src="../../public/login_img/main2.webp" alt="" />
+      </div>
+
+
+      {/* 所有h5展示盒子 */}
+      <div className="all_main_h5_box">
+
+      </div>
+
+
+
+      {/* 广告盒 */}
+      <div className="all_main_ads_box">
+        <img src="../../public/login_img/main1.webp" alt="" />
+      </div>
+      {/* 设计大咖盒 */}
       <div className="all_master_box">
-        <h3>设计大咖<span>加入我们 | 月入百万不是梦!</span><a href="#">加入我们 &lt;</a></h3>
+        <h3>设计大咖<span>加入我们 | 月入百万不是梦!</span><a href="#">加入我们 &gt;</a></h3>
         <div className="Master_box">
           <Master />
         </div>
       </div>
+
+      {/* 尾部 */}
       <div className="all_footer_box">
         <Footer />
       </div>
